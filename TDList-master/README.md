1cbe37f8d05491a31b15bcd1f223e19604f75d8e
abeeel <nabilaismiyatii@gmail.com>
2024-12-13 19:12:03 +0700

    hotel react

---
+++ TDList-master/README.md
@@ -0,0 +1,6 @@
+# TDL
+
+![TDList Poster](https://raw.githubusercontent.com/vishalx360/TDList/master/poster.jpg)
+
+TDList is a 'To Do List" is a Progressive Web App, which uses local-storage API to store content and uses service-worker to get offline functionality.
+
---
+++ src/pages/About.jsx
@@ -0,0 +1,231 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      {/* Spinner Start */}
+      <div
+        id="spinner"
+        className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
+      >
+        <div
+          className="spinner-border text-primary"
+          style={{ width: '3rem', height: '3rem' }}
+          role="status"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      {/* Spinner End */}
+
+      {/* Navbar & Hero Start */}
+      <div className="container-fluid position-relative p-0">
+        <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+          <a href="/" className="navbar-brand p-0">
+            <img src="img/logo.png" alt="Logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span className="fa fa-bars"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto py-0">
+              <a href="/" className="nav-item nav-link">Home</a>
+              <a href="/about" className="nav-item nav-link active">About</a>
+              <a href="/booking" className="nav-item nav-link">Booking</a>
+              <a href="/contact" className="nav-item nav-link">Contact</a>
+            </div>
+            <a href="/register" className="btn btn-primary rounded-pill py-2 px-4">Register</a>
+          </div>
+        </nav>
+        <div className="container-fluid bg-primary py-5 mb-5 hero-header">
+          <div className="container py-5">
+            <div className="row justify-content-center py-5">
+              <div className="col-lg-10 pt-lg-5 mt-lg-5 text-center">
+                <h1 className="display-3 text-white animated slideInDown">About Us</h1>
+                <nav aria-label="breadcrumb">
+                  <ol className="breadcrumb justify-content-center">
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item"><a href="/">Pages</a></li>
+                    <li className="breadcrumb-item text-white active" aria-current="page">About</li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Navbar & Hero End */}
+
+      {/* About Start */}
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-5">
+            <div
+              className="col-lg-6 wow fadeInUp"
+              data-wow-delay="0.1s"
+              style={{ minHeight: '400px' }}
+            >
+              <div className="position-relative h-100">
+                <img
+                  className="img-fluid position-absolute w-100 h-100"
+                  src="img/about.jpg"
+                  alt="About Us"
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+            <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+              <h6 className="section-title bg-white text-start text-primary pe-3">About Us</h6>
+              <h1 className="mb-4">
+                Welcome to <span className="text-primary">Tourist</span>
+              </h1>
+              <p className="mb-4">
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat
+                ipsum et lorem et sit.
+              </p>
+              <p className="mb-4">
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat
+                ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+              </p>
+              <div className="row gy-2 gx-4 mb-4">
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>First Class Flights
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>Handpicked Hotels
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>5 Star Accommodations
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>Latest Model Vehicles
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>150 Premium City Tours
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="mb-0">
+                    <i className="fa fa-arrow-right text-primary me-2"></i>24/7 Service
+                  </p>
+                </div>
+              </div>
+              <a className="btn btn-primary py-3 px-5 mt-2" href="/">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* About End */}
+
+      {/* Footer Start */}
+      <footer className="container-fluid bg-dark text-light footer pt-5 mt-5">
+        <div className="container py-5">
+          <div className="row g-5">
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Company</h4>
+              <a className="btn btn-link" href="/about">About Us</a>
+              <a className="btn btn-link" href="/contact">Contact Us</a>
+              <a className="btn btn-link" href="/privacy">Privacy Policy</a>
+              <a className="btn btn-link" href="/terms">Terms & Condition</a>
+              <a className="btn btn-link" href="/faq">FAQs & Help</a>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Contact</h4>
+              <p className="mb-2">
+                <i className="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-phone-alt me-3"></i>+012 345 67890
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-envelope me-3"></i>info@example.com
+              </p>
+              <div className="d-flex pt-2">
+                <a className="btn btn-outline-light btn-social" href="/">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a className="btn btn-outline-light btn-social" href="/">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a className="btn btn-outline-light btn-social" href="/">
+                  <i className="fab fa-youtube"></i>
+                </a>
+                <a className="btn btn-outline-light btn-social" href="/">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Gallery</h4>
+              <div className="row g-2 pt-2">
+                {[1, 2, 3, 2, 3, 1].map((item, index) => (
+                  <div className="col-4" key={index}>
+                    <img className="img-fluid bg-light p-1" src={`img/package-${item}.jpg`} alt="Gallery" />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Newsletter</h4>
+              <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+              <div className="position-relative mx-auto" style={{ maxWidth: '400px' }}>
+                <input
+                  className="form-control border-primary w-100 py-3 ps-4 pe-5"
+                  type="text"
+                  placeholder="Your email"
+                />
+                <button
+                  type="button"
+                  className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="copyright">
+            <div className="row">
+              <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                &copy; <a className="border-bottom" href="/">Your Site Name</a>, All Right Reserved.
+                <br />
+                Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+              </div>
+              <div className="col-md-6 text-center text-md-end">
+                <div className="footer-menu">
+                  <a href="/">Home</a>
+                  <a href="/cookies">Cookies</a>
+                  <a href="/help">Help</a>
+                  <a href="/faq">FAQs</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* Footer End */}
+
+      {/* Back to Top */}
+      <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top">
+        <i className="bi bi-arrow-up"></i>
+      </a>
+    </>
+  );
+};
+
+export default About;
